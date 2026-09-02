d97dda7259dbfab7b0231ddc150fc905d45393ad
pammelitas <pamelahd.1996@gmail.com>
2026-09-02 10:23:12 -0600

    Lab 1: Project Structure

---
+++ lab1.java
@@ -0,0 +1,18 @@
+// Imports as needed
+import java.util.Scanner;
+
+// MAIN 
+public class lab1 {
+    public static void main(String[] args){
+
+    }
+}
+
+// Chesspiece
+class Chesspiece{
+    public String piece_name;
+    public String color;
+    public String pos_X;
+    public String pos_Y;
+
+}
